a271a6a142dbab33ab17c05c57750130a750f8f2
williamsantosa <williamwsantosa@gmail.com>
2025-05-10 21:58:02 -0700

    font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,11 @@
 	<div
 		class="mx-auto grid h-screen w-screen max-w-[90%] grid-cols-[auto_1fr] items-center gap-20"
 	>
-		<div class="text-neutral-content flex flex-col items-start justify-center gap-1">
+		<div class="text-neutral-content flex flex-col items-start justify-center">
 			<p class="text-base-content text-[72px] font-bold">William Santosa</p>
 			<p class="text-primary-content text-[32px]">Software Engineer</p>
 			<p class="text-primary-content text-[18px]">
-				Hi! I'm a software engineer with experience in infrastructure, database management
-				systems, system design, and configurations. I graduated with my masters from UCLA and graduated UCSC summa cum laude for my bachelors. I have a passion for building scalable and efficient systems, and I'm always looking for new challenges to tackle.
-			</p>
+				Hi! I'm a software engineer with professional experience in infrastructure, database management systems, system design, and configurations. I earned my master's degree in computer science from UCLA and graduated summa cum laude with a bachelor's degree in computer science from UCSC. Beyond work, I enjoy editing and filming videos, rock climbing, hiking, and volunteering.
 		</div>
 		<div class="avatar">
 			<div class="mask mask-squircle h-screen w-[90%]">
