2c5eb879d752f382f62a6c12ad95347939997c81
williamsantosa <williamwsantosa@gmail.com>
2025-05-10 19:58:27 -0700

    starting website dev

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>William Santosa | Home</title>
+	<meta name="description" content="William Santosa - Software Engineer" />
+	<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+	<link rel="apple-touch-icon" href="/favicon.svg" type="image/svg+xml" />
+	<link rel="manifest" href="/manifest.webmanifest" />
+	<meta name="theme-color" content="#000000" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+</svelte:head>
+
+<div class="navbar text-neutral-content bg-transparent fixed">
+	<div class="navbar-start">
+		<button class="text-base-content btn btn-ghost text-xl">William Santosa</button>
+	</div>
+
+	<div class="navbar-center">
+		<a class="text-base-content btn btn-ghost text-xl">daisyUI</a>
+	</div>
+
+	<div class="navbar-end">
+		<button class="text-base-content btn btn-ghost text-xl">Github</button>
+	</div>
+</div>
+
+{@render children()}
+
+<footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+  <aside>
+    <p>© {new Date().getFullYear()} William Santosa - All Rights Reserved</p>
+  </aside>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<div class="h-screen w-screen">
+	<div
+		class="mx-auto grid h-screen w-screen max-w-[90%] grid-cols-[auto_1fr] items-center gap-20"
+	>
+		<div class="text-neutral-content flex flex-col items-start justify-center gap-1">
+			<p class="text-base-content text-[72px] font-bold">William Santosa</p>
+			<p class="text-primary-content text-[32px]">Software Engineer</p>
+			<p class="text-primary-content text-[18px]">
+				Hi! I'm a software engineer with experience in infrastructure, database management
+				systems, system design, and configurations. I graduated with my masters from UCLA and graduated UCSC summa cum laude for my bachelors. I have a passion for building scalable and efficient systems, and I'm always looking for new challenges to tackle.
+			</p>
+		</div>
+		<div class="avatar">
+			<div class="mask mask-squircle h-screen w-[90%]">
+				<img src="/favicon.svg" alt="William Santosa Logo" />
+			</div>
+		</div>
+	</div>
+</div>
